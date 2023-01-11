9d87234d3e92e195f2f2b18587c40370c3af432d
justinkim2015 <justinkim2015@gmail.com>
2023-01-11 20:32:55 +0900

    Edit desktop and ipad version of contact page

--- src/contact/Contact.js
+++ src/contact/Contact.js
@@ -12,22 +12,20 @@ const Contact = () => {
       exit={{opacity: 0}}
     >
 
-      <div className="flex contact-info appear-now">
-        <div className="contact-info-box">
-          <h2 className="contact-header flex" id="contact-header">Contact</h2>
-          <p className="contact-message">
-            If you would like to work together or get in contact I'm available on LinkedIn or via email!
-          </p>
-          <p className="contact-email">
-            Justinjkim2015@gmail.com
-          </p>
-          <p className="contact-email resume">
-            <a href={Resume} download>Resume</a>
-          </p>
-          <div className="contact-icons icons">
-            <a href="https://github.com/justinkim2015" className="devicon-github-original colored icon"></a>
-            <div><a href="https://www.linkedin.com/in/justin-kim-809612ba/" className="devicon-linkedin-plain icon"></a></div>
-          </div>
+      <div className="contact-info-box appear-now">
+        <h2 className="contact-header flex" id="contact-header">Contact</h2>
+        <p className="contact-message">
+          If you would like to work together or get in contact I'm available on LinkedIn or via email!
+        </p>
+        <p className="contact-email">
+          Justinjkim2015@gmail.com
+        </p>
+        <p className="contact-email resume">
+          <a href={Resume} download>Resume</a>
+        </p>
+        <div className="contact-icons icons">
+          <a href="https://github.com/justinkim2015" className="devicon-github-original colored icon"></a>
+          <div><a href="https://www.linkedin.com/in/justin-kim-809612ba/" className="devicon-linkedin-plain icon"></a></div>
         </div>
       </div>
       <Form />
