42aa3cbc637efd821a127a5b749ecbed9481d8a2
justinkim2015 <justinkim2015@gmail.com>
2023-01-11 21:21:33 +0900

    Style projects page

--- src/projects/Project.js
+++ src/projects/Project.js
@@ -1,40 +1,28 @@
 import "./projects.css";
 
 const Project = ({ image, alt, title, live, git, desc, identity }) => {
-
-
   const toggle = () => {
-    let overlayTitle = document.querySelector(`#${identity}-title`)
-    let overlayText = document.querySelector(`#${identity}-text`)
-    overlayTitle.classList.toggle('hidden')
-    overlayText.classList.toggle('hidden')
-  }
+    let overlayTitle = document.querySelector(`#${identity}-title`);
+    let overlayText = document.querySelector(`#${identity}-text`);
+    overlayTitle.classList.toggle("hidden");
+    overlayText.classList.toggle("hidden");
+  };
 
   return (
     <div className="project-card flex">
-      <div className='project-desktop'>
+      <div className="project-desktop-image">
         <div className="overlay" onMouseEnter={toggle} onMouseLeave={toggle}>
-          <h1 className='overlay-title hidden' id={`${identity}-title`} >{title}</h1>
-          <p className='overlay-text hidden' id={`${identity}-text`}>{desc}</p>
+          <h1 className="overlay-title hidden" id={`${identity}-title`}>{title}</h1>
+          {/* Add icons here to live/git */}
+          <p className="overlay-text hidden" id={`${identity}-text`}>{desc}</p>
         </div>
-
-        <div className="project-card-heading">
-            <h3 className='project-title'>{title}</h3>
-            <div className="icon-container">
-              <a href={live}>Live</a>
-              <a
-                href={git}
-                className="devicon-github-original colored icon-small"
-              ></a>
-            </div>
-          </div>
       </div>
 
       <div className="mobile tablet">
         <img src={image} alt={alt} className="project-image"></img>
         <div className="project-body flex">
           <div className="project-card-heading">
-            <h3 className='project-title'>{title}</h3>
+            <h3 className="project-title">{title}</h3>
             <div className="icon-container">
               <a href={live}>Live</a>
               <a
@@ -48,6 +36,6 @@ const Project = ({ image, alt, title, live, git, desc, identity }) => {
       </div>
     </div>
   );
-}
+};
 
 export default Project;
